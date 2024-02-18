9258d0282e556e287e69428795959949eb4ff5b5
Javier Lona <javierlona1@gmail.com>
2024-02-18 13:58:10 -0600

    Simple signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,43 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-signals';
+
+  counter = signal(0);
+
+  derivedCounter = computed(() => {
+    const counter = this.counter();
+    return counter * 10;
+  });
+
+  course = signal({
+    id: 1,
+    title: "Angular for newboobs"
+  });
+
+  courses = signal([
+    "Angular For Beginners",
+    "Reactive Angular Course"
+  ]);
+
+  constructor() {
+
+    effect(() => {
+      const counterValue = this.counter();
+      const derivedCounter = this.derivedCounter();
+
+      console.log(` counter: ${counterValue} derived counter: ${derivedCounter}`);
+    });
+
+  }
+
+  increment() {
+    this.counter.update(val => val + 1);
+
+    this.course.set({
+      id: 1,
+      title: "Hello World"
+    });
+
+    this.courses.update(courses => [...courses, "Angular Core Deep Dive"]);
+  }
 }
