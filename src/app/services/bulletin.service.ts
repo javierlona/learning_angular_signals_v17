517874edcb89d51199c49b3f32c560687100e6db
Javier Lona <javierlona1@gmail.com>
2024-02-18 18:13:56 -0600

    Working signal data service

--- src/app/services/bulletin.service.ts
+++ src/app/services/bulletin.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
 
 @Injectable({
@@ -6,10 +7,22 @@ import { Injectable, signal } from '@angular/core';
 export class BulletinService {
 
   private bulletinSignal = signal(2);
+  private bulletinInfoSignal = signal("Dud");
 
   readonly bulletin = this.bulletinSignal.asReadonly();
+  readonly bulletinInfo = this.bulletinInfoSignal.asReadonly();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log("constructor");
+    setTimeout(() => {
+      http.get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(response => {
+        console.log("Subscribe");
+        this.bulletinInfoSignal.set(JSON.stringify(response));
+      })
+      console.log('5second delay!'); },
+      5000);
+  }
 
   square() {
     this.bulletinSignal.update(val => val * val);
