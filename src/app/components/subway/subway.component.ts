a99a4f298a81c0422c28080dd7ddf0b94a4d24f8
Javier Lona <javierlona1@gmail.com>
2024-02-24 19:39:32 -0600

    Add getPostsFromSpecificUser function

--- src/app/components/subway/subway.component.ts
+++ src/app/components/subway/subway.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { BulletinService } from '../../services/bulletin.service';
 import { Post } from '../../../models/Post';
 
@@ -10,4 +10,23 @@ import { Post } from '../../../models/Post';
   styleUrl: './subway.component.css'
 })
 export class SubwayComponent {
+
+  justBody: string = "";
+  postsByUser = computed(() => {
+    const posts = this.getPostsFromSpecificUser(this.bulletinService.bulletinArray());
+    return posts;
+  });
+
+
+  constructor(public bulletinService: BulletinService) {
+  }
+
+
+  getPostsFromSpecificUser(posts: Post[]) {
+    const filteredPosts = posts.filter((post) => {
+      return post.userId == 4
+    })
+    console.log("getPostsFromSpecificUser", filteredPosts)
+    return filteredPosts;
+  }
 }
