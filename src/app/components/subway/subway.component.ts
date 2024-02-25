93664d4d510745a65b66e5b0f512cf196fb74780
Javier Lona <javierlona1@gmail.com>
2024-02-25 11:31:18 -0600

    Add comments to file

--- src/app/components/subway/subway.component.ts
+++ src/app/components/subway/subway.component.ts
@@ -10,8 +10,17 @@ import { Post } from '../../../models/Post';
   styleUrl: './subway.component.css'
 })
 export class SubwayComponent {
+  postBody: string = "";
+  /**
+   * The line below will not work if postsByUser is not a computed signal
+  */
+  // postsByUser: Post[] = this.bulletinService.bulletinArray()
 
-  justBody: string = "";
+  /**
+   * A computed signal derives its value from other signals,
+   * whenever 'bulletinArray' updates, Angular knows that anything which
+   * depends on it will need to update as well.
+   */
   postsByUser = computed(() => {
     const posts = this.getPostsFromSpecificUser(this.bulletinService.bulletinArray());
     return posts;
@@ -19,6 +28,20 @@ export class SubwayComponent {
 
 
   constructor(public bulletinService: BulletinService) {
+    /**
+     * The console log will display an empty arrary because the information has * not been received from the service, also it is not aware it needs
+     * to update.
+     */
+    console.log("subway constructor", this.bulletinService.bulletinArray())
+    /**
+     * postBody will remain an empty string because it is defined
+     * at runtime and not aware it needs to change.
+     */
+    this.postBody = this.bulletinService.bulletinInfo().body ?? "";
+    /**
+     * The line below will not work if postsByUser is not a computed signal
+     */
+    // this.postsByUser = this.getPostsFromSpecificUser(this.bulletinService.bulletinArray());
   }
 
 
